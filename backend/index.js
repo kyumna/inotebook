55320afa98677c30e8c7f26272406f62ab469d12
Yumna Khan <kyumna14@gmail.com>
2023-04-17 16:53:08 +0500

    models and routes created

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,18 @@ connectToMongo()
 const app = express()
 const port = 3000
 
+//Available Routes
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes.js'))
+
+
 app.get('/', (req, res) => {
   res.send('Bye World!')
 })
 
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
